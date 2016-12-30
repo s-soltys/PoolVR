70d5dea72279fa2fd31475e7a3b9f95e8394c356
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2016-12-30 16:04:10 +0100

    Enable rotating the stage.

---
+++ Assets/PoolVR/Scripts/RotateAround.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+    public Vector3 directionEuler;
+
+	void Update () {
+        transform.Rotate(Time.deltaTime * directionEuler);
+	}
+}
