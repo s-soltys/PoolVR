ae669239845073dbec3095321dc7961e37c2aff3
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2017-01-03 21:27:26 +0100

    Implement additional camera tilt on head movements.

---
+++ Assets/PoolVR/Scripts/TiltOnRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltOnRotate : MonoBehaviour {
+    public Vector2 scale;
+
+	void Update () {
+        var rotation = transform.localRotation.eulerAngles;
+        
+        transform.localPosition = new Vector3(
+            scale.x * Mathf.DeltaAngle(rotation.z, 0) / 180,
+            scale.y * Mathf.DeltaAngle(rotation.x, 0) / 180,
+            0
+            );
+	}
+}
